513ff385a746807b79298e1fa7cb033fd5b847f9
tangjicheng <jicheng.tang@finstadiumx.co.jp>
2024-05-11 13:55:33 +0900

    update log

--- api.py
+++ api.py
@@ -1,10 +1,9 @@
 from fastapi import FastAPI, HTTPException
 from datetime import datetime
 import singleton
-
+import uvicorn
 
 app = FastAPI()
-singleton.logger.info('FDP-SOS is starting up')
 
 @app.get("/api/config")
 async def read_config():
@@ -15,14 +14,25 @@ async def read_config():
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
 
+
 @app.get("/")
 async def read_root():
     return {"message": "Welcome to the FastAPI application!"}
+
 
 @app.get("/add/")
 async def add(a: int, b: int):
     return {"result": a + b}
 
+
 @app.get("/time/")
 async def get_time():
     return {"time": datetime.now().isoformat()}
+
+
+if __name__ == "__main__":
+    singleton.logger.info("FDP-SOS is starting up")
+    singleton.logger.info(f"application config: {singleton.config}")
+    from uvicorn.config import LOGGING_CONFIG
+    LOGGING_CONFIG["formatters"]["default"]["fmt"] = "%(asctime)s    %(levelname)s:    %(message)s"
+    uvicorn.run("api:app", host="0.0.0.0", port=singleton.config["api"]["port"], log_config=LOGGING_CONFIG)
